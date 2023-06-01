35d85042c38bd4dae213de905f343779246c2f15
Madiha Umair <madiha.altaf@yahoo.com>
2023-06-01 12:47:57 -0400

    event handler added to form

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -23,7 +23,7 @@ const profile = () => {
         console.log('Submitted!');
     }
 
-    const handleChange = (event) => {
+    const eventHandler = (event) => {
 
         const { name, options, files } = event.target;
 
@@ -67,37 +67,37 @@ const profile = () => {
                 <form >
                     <section>
                         <label htmlFor="picture">Upload Picture:</label>
-                        <input type="file" id="picture" name="picture" accept="image/*" />
+                        <input type="file" id="picture" name="picture" accept="image/*" onChange={eventHandler} />
                         <label htmlFor="child_name">Child's Name</label>
-                        <input type="text" name="child_name" id="child_name" placeholder="Child Name" required={true} value={profileData.child_name} onChange={handleChange} />
+                        <input type="text" name="child_name" id="child_name" placeholder="Child Name" required={true} value={profileData.child_name} onChange={eventHandler} />
                         <label htmlFor="age">Age</label>
-                        <input type="number" name="age" id="age" placeholder="Age" required={true} value={profileData.age} onChange={handleChange} />
+                        <input type="number" name="age" id="age" placeholder="Age" required={true} value={profileData.age} onChange={eventHandler} />
 
                         <label>Gender</label>
                         <div>
-                            <input type="radio" name="gender" id="girl" required={true} value="girl" onChange={handleChange} checked={profileData.gender === "girl"} />
+                            <input type="radio" name="gender" id="girl" required={true} value="girl" onChange={eventHandler} checked={profileData.gender === "girl"} />
                             <label for="girl">Girl</label>
-                            <input type="radio" name="gender" id="boy" required={true} value="boy" onChange={handleChange} checked={profileData.gender === "boy"} />
+                            <input type="radio" name="gender" id="boy" required={true} value="boy" onChange={eventHandler} checked={profileData.gender === "boy"} />
                             <label for="boy">Boy</label>
-                            <input type="radio" name="gender" id="other" required={true} value="other" onChange={handleChange} checked={profileData.gender === "other"} />
+                            <input type="radio" name="gender" id="other" required={true} value="other" onChange={eventHandler} checked={profileData.gender === "other"} />
                             <label for="other">Other</label>
                         </div>
 
                         <label htmlFor="city">City</label>
-                        <input type="text" name="city" id="city" placeholder="City" required={true} value={profileData.city} onChange={handleChange} />
+                        <input type="text" name="city" id="city" placeholder="City" required={true} value={profileData.city} onChange={eventHandler} />
                         <label htmlFor="country">Country</label>
-                        <input type="text" name="country" id="country" placeholder="Country" required={true} value={profileData.country} onChange={handleChange} />
+                        <input type="text" name="country" id="country" placeholder="Country" required={true} value={profileData.country} onChange={eventHandler} />
                         <label htmlFor="language">Language</label>
-                        <input type="text" name="language" id="language" placeholder="Language" required={true} value={profileData.language} onChange={handleChange} />
+                        <input type="text" name="language" id="language" placeholder="Language" required={true} value={profileData.language} onChange={eventHandler} />
                         <label htmlFor="other_language">Other Language</label>
-                        <input type="text" name="other_language" id="other_language" placeholder="Other Language" value={profileData.other_language} onChange={handleChange} />
+                        <input type="text" name="other_language" id="other_language" placeholder="Other Language" value={profileData.other_language} onChange={eventHandler} />
 
 
                         <label htmlFor="show_matches">Show matches in city:</label>
-                        <input type="text" name="show_matches" id="show_matches" placeholder="City Name" required={true} value={profileData.show_matches} onChange={handleChange} />
+                        <input type="text" name="show_matches" id="show_matches" placeholder="City Name" required={true} value={profileData.show_matches} onChange={eventHandler} />
 
                         <label htmlFor="interest">Interest:</label>
-                        <select name="interest" id="interest" multiple value={profileData.interest} onChange={handleChange} >
+                        <select name="interest" id="interest" multiple value={profileData.interest} onChange={eventHandler} >
                             <option value="sports">Sports</option>
                             <option value="art_and_craft">Art & Craft</option>
                             <option value="music">Music</option>
@@ -110,7 +110,7 @@ const profile = () => {
                         </select>
 
                         <label htmlFor="availability">Availability:</label>
-                        <select name="availability" id="availability" multiple value={profileData.availability} onChange={handleChange} >
+                        <select name="availability" id="availability" multiple value={profileData.availability} onChange={eventHandler} >
                             <option value="weekend">Weekend</option>
                             <option value="weekdays">Weekdays</option>
                             <option value="morning">Morning</option>
@@ -119,7 +119,7 @@ const profile = () => {
                         </select>
 
                         <label htmlFor="additional_info">Additional Information:</label>
-                        <textarea id="additional_info" name="additional_info" rows="4" cols="1" placeholder="Enter any other relevant information here" value={profileData.additional_info} onChange={handleChange}   ></textarea>
+                        <textarea id="additional_info" name="additional_info" rows="4" cols="1" placeholder="Enter any other relevant information here" value={profileData.additional_info} onChange={eventHandler}   ></textarea>
 
                         <input type="submit" />
                     </section>
