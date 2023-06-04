d0bac9a79b0d33edd17ef1f65b9a6fa4e7c9313d
Madiha Umair <madiha.altaf@yahoo.com>
2023-06-04 00:33:15 -0400

    sign up done

--- index.cjs
+++ index.cjs
@@ -1,71 +1,76 @@
-const PORT = 8888;
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const { v4: uuidv4 } = require('uuid');
-const jwt = require ('jsonwebtoken')
-const cors = require ('cors')
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
 const uri = 'mongodb+srv://Madiha:Nissan123!@cluster0.xfsegp5.mongodb.net/playpal-data?retryWrites=true&w=majority';
+const PORT = 8888;
 
 const app = express();
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.json('Hello to my app');
 });
 
-app.post('/signup', async(req, res) => {
-    const client = new MongoClient(uri);
-    const {email, password } = req.body; /*extracting the values of email and password from the request*/
-    const uniqueUserId = uuidv4();
-    const encryptedPassword = await bcrypt.hash(password, 7)
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+  const uniqueUserId = uuidv4();
+  const encryptedPassword = await bcrypt.hash(password, 10); // Changed the number of bcrypt rounds to 10
 
-    try{
-      client.connect()
-      const database = client.db('playpal-data')
-      const users = database.collection('users')
-      const registeredUsers = users.findOne({email})
+  const client = new MongoClient(uri);
 
-      if (registeredUsers) {
-        return res.status(409).send('The user account already exists. Please proceed to login');
-      }
+  try {
+    await client.connect();
+    const database = client.db('playpal-data');
+    const users = database.collection('users');
+    const registeredUser = await users.findOne({ email });
 
-      const filteredEmail = email.tolowerCase();
-      const data = {
-        user_id: uniqueUserId,
-        email: filteredEmail, 
-        hashed_password: encryptedPassword
-      }
-
-      const newUser =await users.insertOne(data)
-
-      const token = jwt.sign(newUser,filteredEmail, {
-        expiresIn : 60*12,
-      })
-      res.status(201).json({token, userId: uniqueUserId, email: filteredEmail})
+    if (registeredUser) {
+      return res.status(409).send('The user account already exists. Please proceed to login.');
     }
-    catch (err) {
-      console.log (err)
-    }
-  });
+
+    const filteredEmail = email.toLowerCase();
+    const data = {
+      user_id: uniqueUserId,
+      email: filteredEmail,
+      hashed_password: encryptedPassword,
+    };
+
+    const newUser = await users.insertOne(data);
+
+    const token = jwt.sign({ userId: newUser.insertedId }, filteredEmail, {
+      expiresIn: '12h',
+    });
+
+    res.status(201).json({ token, userId: uniqueUserId, email: filteredEmail });
+  } catch (err) {
+    console.error(err); // Log the error to the console
+    res.status(500).send('Internal Server Error');
+  } finally {
+    await client.close();
+  }
+});
 
 app.get('/users', async (req, res) => {
-    try {
-      const client = new MongoClient(uri);
-      await client.connect();
-      const database = client.db('playpal-data');
-      const users = database.collection('users');
-      const returnedusers = await users.find().toArray();
-      res.send(returnedusers);
-    } catch (error) {
-      console.error('Error:', error);
-      res.status(500).send(`An error occurred: ${error.message}`);
-    } finally {
-      if (client) {
-        await client.close();
-      }
-    }
-  });
+  const client = new MongoClient(uri);
 
+  try {
+    await client.connect();
+    const database = client.db('playpal-data');
+    const users = database.collection('users');
+    const returnedUsers = await users.find().toArray();
+    res.send(returnedUsers);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send(`An error occurred: ${error.message}`);
+  } finally {
+    await client.close();
+  }
+});
 
 app.listen(PORT, function () {
   console.log('Server listening on port ' + PORT);
--- src/components/AuthenticationForm.jsx
+++ src/components/AuthenticationForm.jsx
@@ -1,60 +1,86 @@
-import {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-let navigate = useNavigate();
+const AuthenticationForm = ({ setShowAuth, isSignup }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
 
+  let navigate = useNavigate();
 
-const AuthenticationForm = ({setShowAuth, isSignup}) => {
-    const [email, setEmail] =useState(null);
-    const [password, setPassword] =useState(null);
-    const [confirmPassword, setConfirmPassword] =useState(null);
-    const [error, setError] =useState(null);
+  const handleClick = () => {
+    setShowAuth(false);
+  };
 
-    console.log(email, password, confirmPassword);
+  const submitFunc = async (e) => {
+    e.preventDefault();
+    try {
+      if (isSignup && password !== confirmPassword) {
+        setError('Password not matching!');
+        return;
+      }
+      const response = await axios.post('http://localhost:8888/signup', {
+        email,
+        password,
+      });
 
-    const handleClick = () => {
-        setShowAuth(false)
+      const success = response.status === 201;
+
+      if (success) navigate('/dashboard');
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  return (
+    <div className="authentication">
+      <div className="close" onClick={handleClick}>
+        [X]
+      </div>
+      <h2>{isSignup ? 'Create Account' : 'Log in'}</h2>
+      <p>
+        By clicking log in, you agree to our terms. Learn how we process your
+        data in our Privacy Policy
+      </p>
+      <form onSubmit={submitFunc} >
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {isSignup && (
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            required
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        )}
+        <button className="form-submit-btn" type="submit">Submit</button>
+        <p>{error}</p>
+      </form>
+      <hr />
+      <h2>GET THE APP</h2>
+    </div>
+  );
+};
 
-    const submitFunc = async(e) => {
-        e.preventDefault()
-        try{
-            if(isSignup && (password !== confirmPassword))
-            {
-                setError("Password not matching!");
-                return
-            }
-            const response = await axios.post('http://localhost:8888/users', {email,password});
-
-            const success = response.status === 201
-
-            if(success) navigate('/dashboard')
-        }
-        catch (error)
-        {
-            console.log("error");
-        }
-    } /*as a default page refreshes we dont want our page refresh we want to stay there so,prevent form from refreshing*/
-
-    return (
-        <div className="authentication">
-            <div className="close" onClick={handleClick}>[X]</div>
-            <h2>{isSignup ? "Create Account" : "Log in"}</h2>
-            <p>By clicking log in, you agree to our terms. Learn how we process your data in our Privacy Policy</p>
-            <form onSubmit={submitFunc}>
-                <input type="email" id="email" name="email" placeholder ="email" required={true} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" id="password" name="password" placeholder ="password" required={true} onChange={(e) => setPassword(e.target.value)} />
-                {isSignup && 
-                <input type="confirmPassword" id="confirmPassword" name="confirmPassword" placeholder ="Confirm Password" required={true} onChange={(e) => setConfirmPassword(e.target.value)} />
-                }
-               <input class="form-submit-btn" type="submit"/>
-               <p>{error}</p>
-            </form>
-            <hr/>
-            <h2>GET THE APP</h2>
-        </div>
-    )
-}
-export default AuthenticationForm
+export default AuthenticationForm;
